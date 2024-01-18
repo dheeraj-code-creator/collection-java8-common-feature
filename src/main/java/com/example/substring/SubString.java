e16d1804d2097d093e5e2a23d8db1ebe0766f4cd
Dheeraj Kumar <dheeraj.10684356@ltimindtree.com>
2024-01-18 14:18:35 +0530

    added substring file

---
+++ src/main/java/com/example/substring/SubString.java
@@ -0,0 +1,10 @@
+package com.example.substring;
+
+public class SubString {
+    public static void main(String[] args) {
+        // print put from the string
+        String str = "computer";
+        String result = str.substring(3, 6);
+        System.out.println("substring of a string is: " + result);
+    }
+}
